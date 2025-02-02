8ee0bc32171b8e417facc2fe376d605ccd4ab188
Kojin Fox <kojinfox@gmail.com>
2025-02-01 22:38:20 -0800

    fix: Add public Firebase configuration
    - Add public-config.js with non-sensitive Firebase configuration
    - Update script imports to use public-config.js

---
+++ public-config.js
@@ -0,0 +1,11 @@
+// Firebase configuration for public use
+const firebaseConfig = {
+    apiKey: "AIzaSyBJL16XmFwU7BAk9ypZuWtz0oPmkA62HvI",
+    authDomain: "construction-career-survey.firebaseapp.com",
+    projectId: "construction-career-survey",
+    storageBucket: "construction-career-survey.appspot.com",
+    messagingSenderId: "1234567890",
+    appId: "1:1234567890:web:abcdef1234567890"
+};
+
+export default firebaseConfig;
