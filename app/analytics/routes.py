8b9cac71368e8f9bad7ba37e1070c5590a81a427
Kojin Fox <kojinfox@gmail.com>
2025-02-03 18:28:59 -0800

    chore: remove unused files and clean up repository

--- app/analytics/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template, flash, redirect, url_for, request
-from flask_login import login_required, current_user
-from app.analytics import bp
-
-@bp.route('/')
-@login_required
-def index():
-    return render_template('analytics/index.html', title='Analytics Dashboard')
--- app/survey/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint('survey', __name__)
-
-from app.survey import routes
